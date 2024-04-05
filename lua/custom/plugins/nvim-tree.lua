58139304e2d5c6ed97eee62f2c0a9528462216e0
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2024-04-05 13:53:08 -0600

    Add nvim tree file explorer

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    -- dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup {
+        hijack_netrw = false,
+      }
+
+      vim.keymap.set('n', '<leader>ee', ':NvimTreeFindFileToggle<CR>')
+      vim.keymap.set('n', '<leader>ef', ':NvimTreeFindFile<CR>')
+    end,
+  },
+}
