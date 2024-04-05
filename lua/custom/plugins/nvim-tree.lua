05625762291a57e679f4fbf1c1565e403a9e3556
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2024-04-05 14:29:30 -0600

    Update nvim-tree config

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,12 +1,11 @@
 return {
   {
     'nvim-tree/nvim-tree.lua',
-    -- dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       require('nvim-tree').setup {
         hijack_netrw = false,
       }
-
       vim.keymap.set('n', '<leader>ee', ':NvimTreeFindFileToggle<CR>')
       vim.keymap.set('n', '<leader>ef', ':NvimTreeFindFile<CR>')
     end,
