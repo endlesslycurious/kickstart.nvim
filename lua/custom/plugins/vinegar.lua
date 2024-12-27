03dc76f8f58dacb08c6aac1682d95af2acf00151
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2024-12-27 11:02:35 -0800

    Add Vinegar file exploerer

---
+++ lua/custom/plugins/vinegar.lua
@@ -0,0 +1,8 @@
+-- Vinegar file explorer
+-- - to open file explorer in current folder
+
+return {
+  -- vinegar - pressing '-' in normal mode will open up file-explorer in
+  -- current folder
+  'tpope/vim-vinegar',
+}
