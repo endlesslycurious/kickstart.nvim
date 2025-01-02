d763bbe7fa25c0266467ec163c959d9eb960afef
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2025-01-02 10:35:03 -0800

    Add key mapping for neotest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -28,5 +28,14 @@ return {
         },
       },
     }
+
+    -- key mappings
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+
+    map('n', '<leader>tn', ":lua require('neotest').run.run()<CR>", opts) -- Run nearest test
+    map('n', '<leader>tf', ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", opts) -- Run file
+    map('n', '<leader>ts', ":lua require('neotest').summary.toggle()<CR>", opts) -- Toggle summary window
+    map('n', '<leader>to', ":lua require('neotest').output.open({ enter = true })<CR>", opts) -- Open test output
   end,
 }
